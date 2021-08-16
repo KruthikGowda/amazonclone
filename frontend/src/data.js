bb4a21c35dc0723dafdadbd9b888e9d7cb1d7d73
Kruthik Gowda <prkruthikgowda@gmail.com>
2021-08-16 16:38:10 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,62 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike slim shirt',
+            category: 'Shirts',
+            images: '/images/p1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            descriptions: 'high quality product',
+
+        },
+        {
+            _id:'2',
+            name:'Adidas shirt',
+            category: 'Shirts',
+            images: '/images/p1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            descriptions: 'high quality product',
+
+        },
+        {
+            _id:'3',
+            name:'Reebok shirt',
+            category: 'Shirts',
+            images: '/images/p1.jpg',
+            price: 150,
+            rating: 4.5,
+            numReviews: 10,
+            descriptions: 'high quality product',
+
+        },
+        {
+            _id:'4',
+            name:'VH shirt',
+            category: 'Shirts',
+            images: '/images/p1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            descriptions: 'high quality product',
+
+        },
+        {
+            _id:'5',
+            name:'Haha shirt',
+            category: 'Shirts',
+            images: '/images/p1.jpg',
+            price: 120,
+            rating: 4.5,
+            numReviews: 10,
+            descriptions: 'high quality product',
+
+        },
+
+    ]
+    
+}
+export default data;
